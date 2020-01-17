4e9ad45db5b49f598669b72383a27a86343bd974
guillermodep <58941444+guillermodep@users.noreply.github.com>
2020-01-17 17:40:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<center><h1>Prueba deploy en alta disponibilidad y deploy A - B</h1</center><br/><br/> \n";
-echo "Hi SRE, This is VERSION 1.0 ------   My Pod IP is : ";
+echo "Hi SRE, This is VERSION 2.0 ------   My Pod IP is : ";
 echo "<p> <font color=blue>";
 echo file_get_contents('/etc/hostname');
 echo "<center><br/> \n";
